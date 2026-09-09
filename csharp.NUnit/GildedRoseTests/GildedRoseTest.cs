f96141be7e9402f6181d86a6ff53bd74ff2011f1
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-09 17:21:11 +0100

    Add SellIn tests

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,20 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
+    public void PositiveItemSellInDecreases()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = "Example", SellIn = 5, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].SellIn, Is.EqualTo(4));
+    }
+    
+    [Test]
+    public void ZeroItemSellInDecreases()
+    {
+        var items = new List<Item> { new Item { Name = "Example", SellIn = 0, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].SellIn, Is.EqualTo(-1));
     }
 }
