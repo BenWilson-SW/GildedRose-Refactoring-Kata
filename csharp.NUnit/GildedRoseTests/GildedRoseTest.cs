50a925cc3625bd7bc7f0182b7bdf090e965b2203
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-09 17:28:25 +0100

    Add ItemQuality tests

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -23,4 +23,40 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.That(items[0].SellIn, Is.EqualTo(-1));
     }
+
+    [Test]
+    public void ItemQualityDecreases()
+    {
+        var items = new List<Item> { new Item { Name = "Example", SellIn = 10, Quality = 10 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(9));
+    }
+    
+    [Test]
+    public void ItemQualityCanNotBeNegative()
+    {
+        var items = new List<Item> { new Item { Name = "Example", SellIn = 10, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void ItemQualityDecreasesNormallyAtSellBy()
+    {
+        var items = new List<Item> { new Item { Name = "Example", SellIn = 10, Quality = 10 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(9));
+    }
+    
+    [Test]
+    public void ItemQualityDecreasesFasterAfterSellBy()
+    {
+        var items = new List<Item> { new Item { Name = "Example", SellIn = -1, Quality = 10 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(8));
+    }
 }
