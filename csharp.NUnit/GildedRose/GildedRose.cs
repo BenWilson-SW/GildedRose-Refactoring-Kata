a721daa0f1063d665ac46dda9b72fc804b158369
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-10 09:43:12 +0100

    Refactor UpdateQuality

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
@@ -15,75 +16,69 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
+            var item = Items[i];
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                continue;                
             }
-
-            if (Items[i].SellIn < 0)
+            
+            int newQuality;
+            switch (item.Name)
             {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
+                case "Aged Brie":
+                    newQuality = GetNewBrieQuality(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    newQuality = GetNewBackstagePassQuality(item);
+                    break;
+                default:
+                    newQuality = GetNewItemQuality(item);
+                    break;
             }
+            
+            item.Quality = Math.Clamp(newQuality, 0, 50);
+            item.SellIn -= 1;
         }
+    }
+
+    private int GetNewBrieQuality(Item brie)
+    {
+        if (brie.SellIn <= 0)
+        {
+            return brie.Quality + 2;
+        }
+
+        return brie.Quality + 1;
+    }
+    
+    private int GetNewBackstagePassQuality(Item backstagePass)
+    {
+        if (backstagePass.SellIn <= 0)
+        {
+            return 0;
+        }
+
+        if (backstagePass.SellIn <= 5)
+        {
+            return backstagePass.Quality + 3;
+        }
+        
+        if (backstagePass.SellIn <= 10)
+        {
+            return backstagePass.Quality + 2;
+        }
+
+        return backstagePass.Quality + 1;
+    }
+
+    private int GetNewItemQuality(Item item)
+    {
+        if (item.SellIn <= 0)
+        {
+            return item.Quality - 2;
+        }
+
+        return item.Quality - 1;
     }
 }
